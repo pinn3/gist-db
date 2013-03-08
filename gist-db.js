10d50fc522cafd2e1363a0aa7d8863b742927758
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2013-03-08 18:24:30 -0500

    starting move to taffydb for the object database

--- gist-db.js
+++ gist-db.js
@@ -1,91 +1,38 @@
 var GitHubApi = require("github");
+var TAFFY = require("taffydb").taffy;
 
 var config = require("./config");
 
 var github = undefined;
 
-var gists = {};
-var groups = {};
+var DB = undefined;
 
 module.exports = function(c){
 
-	config = c;
+	//merge configs
+	//config = c;
 
-	//CONNECT TO GITHUB
-	init();
+	DB = TAFFY([]);
 
-	//CREATE EVENTS
+	//ADD REFRESH FUNCTION TO DB
+	DB.refresh = function(){
 
-	//START EVENT TIMER
-
-	gistDb = {
-		select: select,
-		update: update,
-		insert: insert,
-		delete: remove
 	}
 
-	return gistDb;
-}
-
-
-var init = function(config){
-	//connect to github
+	//CONNECT TO GITHUB
 	github = new GitHubApi({
 	    // required
 	    version: "3.0.0",
 	    // optional
 	    timeout: 5000
 	});
-	updateDB();
+
+	//CREATE EVENTS
+
+	//START EVENT TIMER
+
+	return DB;
 }
 
-var updateDB = function(){
-
-	github.gists.getFromUser({user: "mcwhittemore"}, function(err, res){
-		for(var i=0; i<res.length; i++){
-			manageDB(res[i]);
-		}
-	});
-
-}
-
-var manageDB = function(gist){
-	console.log(Object.keys(gist));
-
-	if(gists[gist.id]==undefined){
-		processGist(gist);
-	}
-	else if(gists[gist.id].updated_at < gist.updated_at){
-		delete gists[gist.id];
-		//SOME CORE TO REMOVE GIST FROM ALL GROUPS
-	}
-
-}
-
-var processGist = function(gist){
-	gists[gist.id] = gist;
-	console.log(gist.id);
-}
 
 /*********************************************************************/
-
-var select = function(where){
-	//4431121
-	console.log("SELECT");
-}
-
-var update = function(where, what){
-	var item = select(where);
-	console.log("UPDATE");
-
-}
-
-var insert = function(what){
-	console.log("INSERT");
-}
-
-var remove = function(where){
-	var item = select(where);
-	console.log("DELETE");
-}
