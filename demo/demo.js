7ac8040e8b502f94437730adf78217a73e5acf23
Jonathan Nguyen <jonathan.nguyen@dooer.com>
2016-06-06 05:36:32 +0200

    Standard format everything

--- demo/demo.js
+++ demo/demo.js
@@ -1,89 +1,85 @@
 var config = {
-	github:{
-		username:"mcwhittemore"
-	}
+  github: {
+    username: 'mcwhittemore'
+  }
 }
 
-var GISTDB = require("../gist-db");
+var GISTDB = require('../gist-db')
 
-var fileInit = function(file){
+var fileInit = function (file) {
 
-  //init custom group object
-  file.groups = {};
+  // init custom group object
+  file.groups = {}
 
-  //define groups and their regex include rules
+  // define groups and their regex include rules
   var group_rules = {
-    "blog" : /^Blog_/,
-    "project": /^Project_/,
-    "icon": /^Icon_$/,
-    "about": /^BlogAboutPage.md$/
-	}
+    'blog': /^Blog_/,
+    'project': /^Project_/,
+    'icon': /^Icon_$/,
+    'about': /^BlogAboutPage.md$/
+  }
 
-  //get group names
-  var groups = Object.keys(group_rules);
+  // get group names
+  var groups = Object.keys(group_rules)
 
-  //set file as excluded as we only want to include it if it has a group
-  var include = false;
+  // set file as excluded as we only want to include it if it has a group
+  var include = false
 
-  for(var i=0; i<groups.length; i++){
-    var group = groups[i];
-    var rule = group_rules[group];
+  for (var i = 0; i < groups.length; i++) {
+    var group = groups[i]
+    var rule = group_rules[group]
 
-    //check if filename matches regex rule
-    if(file.filename.search(rule)>-1){
-      file.groups[group] = true; //set included in group as true
-      include = true; //set include file as true
+    // check if filename matches regex rule
+    if (file.filename.search(rule) > -1) {
+      file.groups[group] = true // set included in group as true
+      include = true // set include file as true
+    }else {
+      file.groups[group] = false
     }
-    else{
-      file.groups[group] = false;
-    }
   }
 
-  if(include){
-    return file;
+  if (include) {
+    return file
+  }else {
+    return undefined
   }
-  else{
-    return undefined;
-  }
-
 }
 
-var fileSave = function(file, callback){
-  file.html = file.raw;
-  callback(file);
+var fileSave = function (file, callback) {
+  file.html = file.raw
+  callback(file)
 }
 
-var _db = GISTDB(config, fileInit, fileSave);
+var _db = GISTDB(config, fileInit, fileSave)
 
-_db.event.on('github_error', function(err, res){
-  console.log("github error");
-  console.log(err);
-  console.log();
-});
+_db.event.on('github_error', function (err, res) {
+  console.log('github error')
+  console.log(err)
+  console.log()
+})
 
-_db.event.on('file_error', function(err, file){
-  console.log("file error");
-  console.log("FILE: "+file.id);
-  console.log("ERROR: "+err.code);
-  console.log();
-});
+_db.event.on('file_error', function (err, file) {
+  console.log('file error')
+  console.log('FILE: ' + file.id)
+  console.log('ERROR: ' + err.code)
+  console.log()
+})
 
-_db.event.on('refreshing', function(){
-  //MIGHT WANT TO LOCK DOWN THINGS FOR A BIT
-  console.log("LETS DO THIS");
-});
+_db.event.on('refreshing', function () {
+  // MIGHT WANT TO LOCK DOWN THINGS FOR A BIT
+  console.log('LETS DO THIS')
+})
 
-_db.event.on('refreshed', function(err){
-  console.log("refresh done");
-  _db().each(function(file){
-    console.log(file.id);
-    if(!file.error){
-      console.log("RAW: "+file.raw.length+" | HTML: "+file.html.length);
-      console.log();
+_db.event.on('refreshed', function (err) {
+  console.log('refresh done')
+  _db().each(function (file) {
+    console.log(file.id)
+    if (!file.error) {
+      console.log('RAW: ' + file.raw.length + ' | HTML: ' + file.html.length)
+      console.log()
+    }else {
+      console.log('ERROR')
+      console.log()
     }
-    else{
-      console.log("ERROR");
-      console.log();
-    }
-  });
-});
+  })
+})
